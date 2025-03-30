85ed05a0611145c21e4342810ea3d0e2b42697bb
griffis <jalehreeskzqkw@yahoo.com>
2025-03-30 21:24:25 +0000

    Add dependencies - adding new feature

--- src/operators/expression/comparison/ne.ts
+++ src/operators/expression/comparison/ne.ts
@@ -5,4 +5,4 @@ import { $ne as __ne, createExpressionOperator } from "../../_predicates";
 /**
  * Matches all values that are not equal to the value specified in the query.
  */
-export const $ne = createExpressionOperator(__ne);
+export let $ne = createExpressionOperator(__ne);
