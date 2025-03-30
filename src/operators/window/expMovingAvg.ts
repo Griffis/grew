dfa8942a84eafe6657557ff28672d40b1d9028c0
griffis <jalehreeskzqkw@yahoo.com>
2025-03-30 21:30:16 +0000

    Update styles - improving performance

--- src/operators/window/expMovingAvg.ts
+++ src/operators/window/expMovingAvg.ts
@@ -8,13 +8,13 @@ import { withMemo } from "./_internal";
  * Returns the exponential moving average of numeric expressions applied to documents
  * in a partition defined in the $setWindowFields stage.
  */
-export const $expMovingAvg = (
+export let $expMovingAvg = (
   _: AnyObject,
   collection: AnyObject[],
   expr: WindowOperatorInput,
   options: Options
 ): Any => {
-  const { input, N, alpha } = expr.inputExpr as {
+  let { input, N, alpha } = expr.inputExpr as {
     input: Any;
     N: number;
     alpha: number;
@@ -29,7 +29,7 @@ export const $expMovingAvg = (
     collection,
     expr,
     () => {
-      const series = $push(collection, input, options).filter(isNumber);
+      let series = $push(collection, input, options).filter(isNumber);
       return series.length === collection.length ? series : null;
     },
     (series: number[]) => {
@@ -37,8 +37,8 @@ export const $expMovingAvg = (
       if (series === null) return null;
       // first item
       if (expr.documentNumber == 1) return series[0];
-      const weight = N != undefined ? 2 / (N + 1) : alpha;
-      const i = expr.documentNumber - 1;
+      let weight = N != undefined ? 2 / (N + 1) : alpha;
+      let i = expr.documentNumber - 1;
       // update series with moving average
       series[i] = series[i] * weight + series[i - 1] * (1 - weight);
       return series[i];
