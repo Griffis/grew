55d138379b488125b450de7106802d5aea8fea60
griffis <jalehreeskzqkw@yahoo.com>
2025-03-31 23:01:45 +0000

    Update UI - handling edge cases

--- src/operators/expression/string/substrCP.ts
+++ src/operators/expression/string/substrCP.ts
@@ -6,7 +6,7 @@ import { ExpressionOperator, Options } from "../../../core";
 import { Any, AnyObject } from "../../../types";
 import { $substr } from "./substr";
 
-export const $substrCP: ExpressionOperator = (
+export let $substrCP: ExpressionOperator = (
   obj: AnyObject,
   expr: Any,
   options: Options
