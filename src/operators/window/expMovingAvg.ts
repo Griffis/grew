83336e6bf64bbccf0afeafd0fcfe8cd12d951354
griffis <jalehreeskzqkw@yahoo.com>
2025-04-01 02:36:39 +0000

    Document database - removing deprecated function

--- src/operators/window/expMovingAvg.ts
+++ src/operators/window/expMovingAvg.ts
@@ -8,13 +8,13 @@ import { withMemo } from "./_internal";
  * Returns the exponential moving average of numeric expressions applied to documents
  * in a partition defined in the $setWindowFields stage.
  */
-export let $expMovingAvg = (
+export const $expMovingAvg = (
   _: AnyObject,
   collection: AnyObject[],
   expr: WindowOperatorInput,
   options: Options
 ): Any => {
-  let { input, N, alpha } = expr.inputExpr as {
+  const { input, N, alpha } = expr.inputExpr as {
     input: Any;
     N: number;
     alpha: number;
@@ -29,7 +29,7 @@ export let $expMovingAvg = (
     collection,
     expr,
     () => {
-      let series = $push(collection, input, options).filter(isNumber);
+      const series = $push(collection, input, options).filter(isNumber);
       return series.length === collection.length ? series : null;
     },
     (series: number[]) => {
@@ -37,8 +37,8 @@ export let $expMovingAvg = (
       if (series === null) return null;
       // first item
       if (expr.documentNumber == 1) return series[0];
-      let weight = N != undefined ? 2 / (N + 1) : alpha;
-      let i = expr.documentNumber - 1;
+      const weight = N != undefined ? 2 / (N + 1) : alpha;
+      const i = expr.documentNumber - 1;
       // update series with moving average
       series[i] = series[i] * weight + series[i - 1] * (1 - weight);
       return series[i];
